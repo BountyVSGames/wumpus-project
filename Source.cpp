94266f48cd816228475f44ae7cb1dcf2d50d2382
YoeyT <yoey.tolboom@student.hu.nl>
2020-03-26 13:21:03 +0100

    fixed adjacent rooms

--- Source.cpp
+++ Source.cpp
@@ -9,14 +9,14 @@ struct room {
 	bool pit = false;
 };
 
-room room1{ 1, { 1, 3, 4 } };
-room room2{ 2, { 2, 4, 5 } };
-room room3{ 3, { 2, 3, 5 } };
-room room4{ 4, { 3, 4, 6 } };
-room room5{ 5, { 5, 7, 8 } };
-room room6{ 6, { 1, 6, 8 } };
-room room7{ 7, { 1, 6, 7 } };
-room room8{ 8, { 2, 7, 8 } };
+room room1{ 1, { 8, 2, 3 } };
+room room2{ 2, { 1, 3, 4 } };
+room room3{ 3, { 1, 2, 4 } };
+room room4{ 4, { 2, 3, 5 } };
+room room5{ 5, { 4, 6, 7 } };
+room room6{ 6, { 5, 7, 8 } };
+room room7{ 7, { 5, 6, 8 } };
+room room8{ 8, { 1, 6, 7 } };
 
 int main() {
 	return 0;
