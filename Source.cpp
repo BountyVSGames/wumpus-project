78e66b8d24bb29648d8e3e375007cf906c408732
YoeyT <yoey.tolboom@student.hu.nl>
2020-03-26 14:06:44 +0100

    added vector rooms

--- Source.cpp
+++ Source.cpp
@@ -17,6 +17,7 @@ room room5{ 5, { 4, 6, 7 } };
 room room6{ 6, { 5, 7, 8 } };
 room room7{ 7, { 5, 6, 8 } };
 room room8{ 8, { 1, 6, 7 } };
+vector<room> rooms = {room1, room2 , room3 ,room4, room5, room6, room7, room8};
 
 int main() {
 	return 0;
