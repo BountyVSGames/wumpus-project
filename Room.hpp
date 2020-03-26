3fe0aa3ade6162869aa6177c9baabb08c9017707
wouterdijk <dijk.wouter@yahoo.nl>
2020-03-26 14:18:33 +0100

    test

---
+++ Room.hpp
@@ -0,0 +1,11 @@
+#ifndef ROOM_HPP
+#define ROOM_HPP
+
+void EnterRoom(/*struct room*/);
+void RoomSelection(/*struct room*/);
+
+void BatRoom();
+void WumpusRoom();
+void PitRoom();
+
+#endif // ROOM_HPP
