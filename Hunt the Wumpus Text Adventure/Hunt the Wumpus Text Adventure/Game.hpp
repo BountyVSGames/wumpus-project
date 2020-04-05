7449d622dc7e77712146884c9f4e42ecaf600f94
Jordieboyz <60455414+Jordieboyz@users.noreply.github.com>
2020-04-05 14:11:33 +0200

    verwijdere

--- Hunt the Wumpus Text Adventure/Hunt the Wumpus Text Adventure/Game.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void GameStart(player& currentRoom);
-void GameOver(player& currentRoom, const bool& gameWon);
-
-void PlayerInteraction(player& currentRoom);
-void PlayerShoot(player& currentRoom);
