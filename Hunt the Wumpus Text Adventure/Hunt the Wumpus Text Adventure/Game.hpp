ed4ffc54e0bd42f2eb566e490acc07c4956e59ff
Jordieboyz <60455414+Jordieboyz@users.noreply.github.com>
2020-04-05 14:22:29 +0200

    remove

--- Hunt the Wumpus Text Adventure/Hunt the Wumpus Text Adventure/Game.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-bool GameStart(player& currentRoom);
-void GameOver(player& currentRoom, const bool& gameWon);
-
-void PlayerInteraction(player& currentRoom);
-void PlayerShoot(player& currentRoom);
